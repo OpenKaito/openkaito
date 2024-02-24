fb4e3129739c47066ef645fa07676c2f3b6ffc22
Han Yang <yanghan.cs@outlook.com>
2024-02-24 13:54:42 +0000

    feat: add get_tweets_by_urls method

--- otika/crawlers/twitter/apify.py
+++ otika/crawlers/twitter/apify.py
@@ -14,6 +14,38 @@ class ApifyTwitterCrawler:
         # users may use any other actor id that can crawl twitter data
         self.actor_id = "microworlds/twitter-scraper"
 
+    def get_tweets_by_urls(self, urls: list):
+        """
+        Get tweets by urls.
+
+        Args:
+            urls (list): The list of urls to get tweets from.
+
+        Returns:
+            list: The list of tweets.
+        """
+        bt.logging.debug(f"Getting tweets by urls: {urls}")
+        params = {
+            "maxRequestRetries": 3,
+            "searchMode": "live",
+            "scrapeTweetReplies": False,
+            "urls": urls,
+            "maxTweets": len(urls),
+        }
+
+        run = self.client.actor(self.actor_id).call(
+            run_input=params, timeout_secs=self.timeout_secs
+        )
+        bt.logging.debug(f"Apify Actor Run: {run}")
+
+        results = [
+            item
+            for item in self.client.dataset(run["defaultDatasetId"]).iterate_items()
+        ]
+        bt.logging.trace(f"Apify Results: {results}")
+
+        return results
+
     def search(self, query: str, max_length: int):
         """
         Searches for the given query on the crawled data.
@@ -65,7 +97,9 @@ class ApifyTwitterCrawler:
                 "url": result["url"],
                 "username": result["user"]["screen_name"],
                 "text": result.get("full_text"),
-                "created_at": datetime.strptime(result.get("created_at"), time_format).isoformat(),
+                "created_at": datetime.strptime(
+                    result.get("created_at"), time_format
+                ).isoformat(),
                 "quote_count": result.get("quote_count"),
                 "reply_count": result.get("reply_count"),
                 "retweet_count": result.get("retweet_count"),
@@ -84,7 +118,13 @@ if __name__ == "__main__":
     load_dotenv()
     crawler = ApifyTwitterCrawler(os.environ["APIFY_API_KEY"])
 
-    r = crawler.search("BTC", 5)
+    # r = crawler.search("BTC", 5)
+    # print(crawler.process(r))
 
-    # print(r)
+    r = crawler.get_tweets_by_urls(
+        [
+            "https://twitter.com/VitalikButerin/status/1759369749887332577",
+            "https://twitter.com/elonmusk/status/1760504129485705598",
+        ]
+    )
     print(crawler.process(r))
