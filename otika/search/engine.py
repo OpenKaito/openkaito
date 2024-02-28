65ec82e1c00638ff3457496bef5c938de3bdc635
Han Yang <yanghan.cs@outlook.com>
2024-02-28 09:17:41 +0000

    fix: process

--- otika/search/engine.py
+++ otika/search/engine.py
@@ -2,11 +2,6 @@ import os
 
 import bittensor as bt
 from dotenv import load_dotenv
-from elasticsearch import Elasticsearch
-
-from otika.crawlers.twitter.apify import ApifyTwitterCrawler
-
-from .ranking import HeuristicRankingModel
 
 
 class SearchEngine:
@@ -121,7 +116,7 @@ class SearchEngine:
             )
         try:
             docs = self.twitter_crawler.search(query_string, max_size)
-            processed_docs = self.twitter_crawler.process(docs)
+            processed_docs = self.twitter_crawler.process_list(docs)
             bt.logging.debug(f"processed {len(processed_docs)} docs: {processed_docs}")
         except Exception as e:
             bt.logging.error("crawling error...", e)
