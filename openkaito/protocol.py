8e85f49bf36b470eea4af0fc446322cb310d6a62
Han Yang <yanghan.cs@outlook.com>
2024-03-05 10:59:33 +0000

    fix: add constraints for size in protocol and enlarge default miner search_recall_size to 50

--- openkaito/protocol.py
+++ openkaito/protocol.py
@@ -38,13 +38,13 @@ class SearchSynapse(bt.Synapse):
 
     Attributes:
     - query_string: A string value representing the search request sent by the validator.
-    - size: the count size of results to return.
+    - size: the maximal count size of results to return.
     - version: A `Version` object representing the version of the protocol.
     - results: A list of `Document` dict which, when filled, represents the response from the miner.
     """
 
     query_string: str
-    size: int
+    size: int = pydantic.Field(5, ge=1, le=50)
     version: Optional[Version] = None
 
     results: Optional[List[Dict]] = None
