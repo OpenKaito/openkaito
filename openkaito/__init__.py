9e9c6272be2d70088abd05a1b17fa52df0e8216c
Han Yang <yanghan.cs@outlook.com>
2024-03-19 10:00:36 +0000

    chore: bump version

--- openkaito/__init__.py
+++ openkaito/__init__.py
@@ -15,7 +15,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-__version__ = "0.1.18"
+__version__ = "0.2.0"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
