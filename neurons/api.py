ded945fae50f0c5649cabd852f60438f52503ee7
Han Yang <yanghan.cs@outlook.com>
2024-06-13 09:41:52 +0000

    fix: event loop issue

--- neurons/api.py
+++ neurons/api.py
@@ -13,8 +13,18 @@ from openkaito.utils.config import check_config, add_args, config
 
 from openkaito.base.neuron import BaseNeuron
 
+from openkaito.protocol import (
+    DiscordSearchSynapse,
+    StructuredSearchSynapse,
+    SemanticSearchSynapse,
+)
 
-class ApiNeuron(BaseNeuron):
+from openkaito.utils.uids import get_random_uids
+from openkaito.utils.version import get_version
+from openkaito.utils.misc import ttl_get_block
+
+
+class ApiNeuron(object):
     """
     API node for storage network
 
@@ -39,6 +49,10 @@ class ApiNeuron(BaseNeuron):
     def config(cls):
         return config(cls)
 
+    @property
+    def block(self):
+        return ttl_get_block(self)
+
     subtensor: "bt.subtensor"
     wallet: "bt.wallet"
     metagraph: "bt.metagraph"
@@ -46,38 +60,23 @@ class ApiNeuron(BaseNeuron):
     def __init__(self):
         self.config = ApiNeuron.config()
         self.check_config(self.config)
-        bt.logging(config=self.config, logging_dir=self.config.neuron.full_path)
+        bt.logging(config=self.config.logging)
         bt.logging.debug(self.config)
 
-        bt.logging.debug("loading subtensor")
         self.subtensor = bt.subtensor(config=self.config)
         bt.logging.debug(str(self.subtensor))
 
-        bt.logging.debug("loading wallet")
         self.wallet = bt.wallet(config=self.config)
-
-        if not self.subtensor.is_hotkey_registered_on_subnet(
-            hotkey_ss58=self.wallet.hotkey.ss58_address, netuid=self.config.netuid
-        ):
-            raise Exception(
-                f"Wallet not currently registered on netuid {self.config.netuid}, please first register wallet before running"
-            )
-
         bt.logging.debug(f"wallet: {str(self.wallet)}")
 
-        bt.logging.debug("init metagraph")
-        self.metagraph = bt.metagraph(
-            netuid=self.config.netuid, network=self.subtensor.network, sync=False
-        )
-        self.metagraph.sync(subtensor=self.subtensor)  # Sync metagraph with subtensor.
-        bt.logging.debug(str(self.metagraph))
+        self.metagraph = self.subtensor.metagraph(self.config.netuid)
+        bt.logging.info(f"Metagraph: {self.metagraph}")
 
-        self.my_subnet_uid = self.metagraph.hotkeys.index(
-            self.wallet.hotkey.ss58_address
+        self.uid = self.metagraph.hotkeys.index(self.wallet.hotkey.ss58_address)
+        bt.logging.info(
+            f"Running neuron on subnet: {self.config.netuid} with uid {self.uid} using network: {self.subtensor.chain_endpoint}"
         )
-        bt.logging.info(f"Running validator on uid: {self.my_subnet_uid}")
 
-        bt.logging.debug("serving ip to chain...")
         try:
             self.axon = bt.axon(wallet=self.wallet, config=self.config)
 
@@ -86,32 +85,18 @@ class ApiNeuron(BaseNeuron):
                 blacklist_fn=self.blacklist_semantic_search,
                 priority_fn=self.priority_semantic_search,
             )
-
-            try:
-                self.subtensor.serve_axon(
-                    netuid=self.config.netuid,
-                    axon=self.axon,
-                )
-                self.axon.start()
-
-            except Exception as e:
-                bt.logging.error(f"Failed to serve Axon: {e}")
-                pass
-
+            bt.logging.info(f"Axon created: {self.axon}")
         except Exception as e:
             bt.logging.error(f"Failed to create Axon initialize: {e}")
             pass
 
-        bt.logging.debug("loading dendrite")
         self.dendrite = bt.dendrite(wallet=self.wallet)
         bt.logging.debug(str(self.dendrite))
 
-        # Init the event loop.
         self.loop = asyncio.get_event_loop()
 
         self.last_sync_block = self.block - 10
 
-        # Instantiate runners
         self.should_exit: bool = False
         self.is_running: bool = False
         self.thread: threading.Thread = None
@@ -120,19 +105,31 @@ class ApiNeuron(BaseNeuron):
         self.step = 0
 
     def run(self):
-        self.sync()
+        self.resync_metagraph()
+        try:
+            loop = asyncio.get_event_loop()
+            print("loop", loop)
+        except RuntimeError as e:
+            if str(e).startswith("There is no current event loop in thread"):
+                print("Creating new event loop")
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+            else:
+                print("Error event loop", e)
+                raise
 
-        # Serve passes the axon information to the network + netuid we are hosting on.
-        # This will auto-update if the axon port of external ip have changed.
         bt.logging.info(
             f"Serving miner axon {self.axon} on network: {self.config.subtensor.chain_endpoint} with netuid: {self.config.netuid}"
         )
-        self.axon.serve(netuid=self.config.netuid, subtensor=self.subtensor)
+        try:
+            self.axon.serve(netuid=self.config.netuid, subtensor=self.subtensor)
 
-        # Start  starts the miner's axon, making it active on the network.
-        self.axon.start()
+            self.axon.start()
 
-        bt.logging.info(f"Miner starting at block: {self.block}")
+            bt.logging.info(f"Miner starting at block: {self.block}")
+        except Exception as e:
+            bt.logging.error(f"Failed to start miner: {e}")
+            self.should_exit = True
 
         # This loop maintains the miner's operations until intentionally stopped.
         try:
@@ -148,7 +145,7 @@ class ApiNeuron(BaseNeuron):
                         break
 
                 # Sync metagraph and potentially set weights.
-                self.sync()
+                self.resync_metagraph()
                 self.step += 1
 
         # If someone intentionally stops the miner, it'll safely terminate operations.
@@ -169,7 +166,7 @@ class ApiNeuron(BaseNeuron):
 
     def run_in_background_thread(self):
         if not self.is_running:
-            bt.logging.debug("Starting miner in background thread.")
+            bt.logging.debug("Starting API Neuron in background thread.")
             self.should_exit = False
             self.thread = threading.Thread(target=self.run, daemon=True)
             self.thread.start()
@@ -178,22 +175,100 @@ class ApiNeuron(BaseNeuron):
 
     def stop_run_thread(self):
         if self.is_running:
-            bt.logging.debug("Stopping miner in background thread.")
+            bt.logging.debug("Stopping API Neuron in background thread.")
             self.should_exit = True
             self.thread.join(5)
             self.is_running = False
             bt.logging.debug("Stopped")
 
-    async def forward(self, synapse: bt.Synapse) -> bt.Synapse:
-        bt.logging.warning("forward()")
-        return synapse
-    
+    async def forward_semantic_search(
+        self, query: SemanticSearchSynapse
+    ) -> SemanticSearchSynapse:
+
+        bt.logging.debug(f"Forwarding semantic search query: {query.json()}")
+
+        # TODO: random select miners or top miners? (if top miners, will they be queried too much?)
+        miner_uids = get_random_uids(self, k=self.config.neuron.sample_size)
+
+        responses = await self.dendrite(
+            # Send the query to selected miner axons in the network.
+            axons=[self.metagraph.axons[uid] for uid in miner_uids],
+            synapse=query,
+            deserialize=True,
+            timeout=query.timeout if query.timeout else 10,
+        )
+        # do we run evaluation for api queries?
+
+        # TODO: how to structure the results? (e.g. top 5 results(then we will need to do evaluatoin), or all results?)
+        # How to respect the `size` parameter in synapse?
+        query.results = dict(zip(miner_uids, responses))
+
+        return query
+
+    async def blacklist(self, synapse: bt.Synapse) -> typing.Tuple[bool, str]:
+
+        return False, "Debug mode"
+
+        # Default to deny all hotkeys that are not in the whitelist.
+        return True, "Only whitelisted hotkeys are allowed"
+
+    async def priority(self, synapse: bt.Synapse) -> float:
+        caller_uid = self.metagraph.hotkeys.index(
+            synapse.dendrite.hotkey
+        )  # Get the caller index.
+        prirority = float(
+            self.metagraph.S[caller_uid]
+        )  # Return the stake as the priority.
+        bt.logging.trace(
+            f"Prioritizing {synapse.dendrite.hotkey} with value: ", prirority
+        )
+        return prirority
+
+    async def blacklist_structured_search(
+        self, synapse: StructuredSearchSynapse
+    ) -> typing.Tuple[bool, str]:
+        return await self.blacklist(synapse)
+
+    async def priority_structured_search(
+        self, synapse: StructuredSearchSynapse
+    ) -> float:
+        return await self.priority(synapse)
+
+    async def blacklist_semantic_search(
+        self, synapse: SemanticSearchSynapse
+    ) -> typing.Tuple[bool, str]:
+        return await self.blacklist(synapse)
+
+    async def priority_semantic_search(self, synapse: SemanticSearchSynapse) -> float:
+        return await self.priority(synapse)
+
+    async def blacklist_discord_search(
+        self, synapse: DiscordSearchSynapse
+    ) -> typing.Tuple[bool, str]:
+        return await self.blacklist(synapse)
+
+    async def priority_discord_search(self, synapse: DiscordSearchSynapse) -> float:
+        return await self.priority(synapse)
+
+    def resync_metagraph(self):
+        """Resyncs the metagraph and updates the hotkeys and moving averages based on the new metagraph."""
+        bt.logging.info("resync_metagraph()")
+
+        # Sync the metagraph.
+        self.metagraph.sync(subtensor=self.subtensor)
+        self.last_sync_block = self.block
+        bt.logging.info("resync_metagraph() done")
+
     def __enter__(self):
         self.run_in_background_thread()
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.stop_run_thread()
 
 
 if __name__ == "__main__":
-    ApiNeuron().run()
+    with ApiNeuron() as api_neuron:
+        while True:
+            time.sleep(30)
+            bt.logging.debug(f"API neuron running at block {api_neuron.block}")
