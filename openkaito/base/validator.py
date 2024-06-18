0cf843f2b267f677cd9fac522e7eb8ee61fdbae9
Han Yang <yanghan.cs@outlook.com>
2024-06-18 05:43:37 +0000

    feat: upgrade bittensor

--- openkaito/base/validator.py
+++ openkaito/base/validator.py
@@ -49,7 +49,7 @@ class BaseValidatorNeuron(BaseNeuron):
 
         # Set up initial scoring weights for validation
         bt.logging.info("Building validation weights.")
-        self.scores = torch.zeros_like(self.metagraph.S, dtype=torch.float32)
+        self.scores = torch.zeros(self.metagraph.S.shape, dtype=torch.float32)
 
         self.load_state()
 
